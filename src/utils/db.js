fc8d777bbad81b9e6432155aee0cceb69599ff29
Benjamin Lee <69630292+BenjaminLee-boop@users.noreply.github.com>
2021-09-29 15:40:16 +0930

    Update db.js
    Added Objection connection string

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,5 @@
-require('dotenv').config();
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  },
-});
+const { Model } = require('objection');
 
-module.exports = knex;
+const knex = require('../../knexfile');
+
+module.exports = Model.Knex(knex);
